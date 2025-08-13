cbca3d0fbe89b40642ea089e8543ceadbc4e08b0
yogeshKumar-malairaj <yoogesh06@gmail.com>
2025-08-14 01:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
@@ -6,25 +5,24 @@ from routes.auth import auth_bp
 from routes.contacts import contacts_bp
 from routes.messages import messages_bp
 from config import FRONTEND_ORIGINS, PORT
-from extensions import socketio
 
-def create_app(): 
+socketio = SocketIO(cors_allowed_origins="*")
+
+def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True, origins=FRONTEND_ORIGINS)
 
-    socketio = SocketIO(app, cors_allowed_origins="*")
-    app.socketio = socketio
+    socketio.init_app(app)
 
     # Blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(contacts_bp)
     app.register_blueprint(messages_bp)
-    
-    return app, socketio   # <-- Add this
 
-    
-    
+    return app
+
+# Create the app object for Gunicorn
+app = create_app()
 
 if __name__ == "__main__":
-    app, socketio = create_app()
     socketio.run(app, host="0.0.0.0", port=PORT, debug=True)
